f8e5f699470e1dc5bc5ab3acc142cbf112388252
LKDlovepreet <singhlovepreet05198@gmail.com>
2025-01-12 13:18:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,9 +36,9 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6261595158"))
 #port set to default 8080
 PORT = os.environ.get("PORT", "8080")
 #your database url mongodb only You can use mongo atlas free cloud database
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://sa8619:dsafb@cluster0.9uhmlry.mongodb.net/")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://gangstertown3dbeat:gangstertown3dbeat@cluster0.ny8rz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 #your database name
-DB_NAME = os.environ.get("DB_NAME", "filestorebot")
+DB_NAME = os.environ.get("DB_NAME", "cluster0")
 
 #for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
